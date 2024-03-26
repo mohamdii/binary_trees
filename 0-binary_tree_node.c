76124dae46af11d28acc2193ea96355a8ddade44
mohamdii <m.hamdy97@gmail.com>
2024-03-26 11:06:59 +0200

    first ez task

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates the first node
+ * @parent: the parent node to be created
+ * @value: the assigned value in the node
+*/
+
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if(new == NULL)
+	{
+		return NULL;
+	}
+	new->parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
+	return new;
+}
