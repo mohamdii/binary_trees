48ac820026e232f5d94d315faa0c4d218e641887
mohamdii <m.hamdy97@gmail.com>
2024-03-27 02:20:45 +0200

    adding nulls

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,9 +22,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     new->n = value;
     new->left = parent->left;
     new->right = NULL;
+    
     if (parent->left != NULL)
+    {
         parent->left->parent = new;
-
+        new->left->left = NULL;
+    }
     parent->left = new;
     return (new); 
 }
